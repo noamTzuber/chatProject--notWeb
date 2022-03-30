9b31fb540edb82e638fc7a9143bda08fdea8c0b7
itayshwartz1 <itay2007@gmail.com>
2022-03-30 20:12:39 +0300

    Login and Register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
+import Register from './register/Register';
 import Login from './login/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Login/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/Register'
+            element={<Register />}></Route>
+
+          <Route path='/' element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
