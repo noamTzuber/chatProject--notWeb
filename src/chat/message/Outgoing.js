089c76560a914865f52eda9b19a2b36a2f639599
noamTzuber <noamtooby@gmail.com>
2022-03-31 14:20:28 +0300

    not finished

--- src/chat/message/Outgoing.js
+++ src/chat/message/Outgoing.js
@@ -2,9 +2,15 @@ import './Outgoing.css'
 
 function Outgoing() {
     return (
+
+       
         <div class="Outgoing">
          hey...
         </div>
+        
+        
+        
+        
     );
 }
 export default Outgoing
