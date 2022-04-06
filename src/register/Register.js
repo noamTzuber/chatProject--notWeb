0da378a3d4aa346eef959eabb7cca4257bbd6c2f
itayshwartz1 <itay2007@gmail.com>
2022-04-07 00:51:41 +0300

    register validation, register and login from pixel to %. add icon to add convesation.

--- src/register/Register.js
+++ src/register/Register.js
@@ -3,37 +3,42 @@ import './Register.css'
 function Register() {
 
     function validation() {
-        document.getElementById("error-message").innerHTML = ""
+        var errorMessage = document.getElementById("error-message");
+        errorMessage.innerHTML = ""
 
         let userName = document.getElementById("userName").value;
         if (userName.length === 0) {
-            document.getElementById("error-message").innerHTML += "please enter Username<br>"
+            errorMessage.innerHTML += "please enter Username<br>"
         } else if (/[^a-zA-Z0-9]/.test(userName)) {
-            document.getElementById("error-message").innerHTML += "Use only English and numbers in Username<br>";
+            errorMessage.innerHTML += "Use only English and numbers in Username<br>";
         }
 
         let nickname = document.getElementById("nickname").value;
         if (nickname.length === 0) {
-            document.getElementById("error-message").innerHTML += "please enter Nickname<br>"
+            errorMessage.innerHTML += "please enter Nickname<br>"
         } else if (/[^a-zA-Z0-9]/.test(nickname)) {
-            document.getElementById("error-message").innerHTML += "Use only English and numbers in Nickname<br>";
+            errorMessage.innerHTML += "Use only English and numbers in Nickname<br>";
         }
 
 
         let password = document.getElementById("password").value;
         if (password.length < 8)
-            document.getElementById("error-message").innerHTML += "Use 8 characters or more for your password<br>"
+            errorMessage.innerHTML += "Use 8 characters or more for your password<br>"
         else {
             if (!/[0-9]/.test(password))
-                document.getElementById("error-message").innerHTML += "Use numbers for your password<br>"
+                errorMessage.innerHTML += "Use numbers for your password<br>"
             if (!/[a-zA-Z]/.test(password))
-                document.getElementById("error-message").innerHTML += "Use letters for your password<br>"
+                errorMessage.innerHTML += "Use letters for your password<br>"
 
             if (/[^(a-zA-Z0-9)]/.test(password))
-                document.getElementById("error-message").innerHTML += "Use only English and numbers for your password<br>"
+                errorMessage.innerHTML += "Use only English and numbers for your password<br>"
 
             if (document.getElementById("password-confirm").value !== password)
-                document.getElementById("error-message").innerHTML += "the confirm password is not match<br>"
+                errorMessage.innerHTML += "the confirm password is not match<br>"
+        }
+
+        if (errorMessage.innerHTML === "") {
+            console.log('s')
         }
 
 
@@ -45,35 +50,33 @@ function Register() {
             <span className="fs-1" style={{padding: '2%', display: 'table', margin: '0 auto'}}>Register</span>
 
             <div className='inputAndTitle'>
-                <div class="fs-6">Username</div>
-                <input id='userName' type="text" class="form-control" placeholder="Type your Username"/>
+                <div className="fs-6">Username</div>
+                <input id='userName' type="text" className="form-control" placeholder="Type your Username"/>
             </div>
 
             <div className='inputAndTitle'>
-                <div class="fs-6">Nickname</div>
-                <input id='nickname' type="text" class="form-control" placeholder="Type your Nickname"/>
+                <div className="fs-6">Nickname</div>
+                <input id='nickname' type="text" className="form-control" placeholder="Type your Nickname"/>
             </div>
 
             <div className='inputAndTitle'>
-                <div class="fs-6">Password</div>
-                <input id='password' type="password" class="form-control" placeholder="Type your Password"/>
+                <div className="fs-6">Password</div>
+                <input id='password' type="password" className="form-control" placeholder="Type your Password"/>
             </div>
 
             <div className='inputAndTitle'>
-                <div class="fs-6">Confirm Password</div>
+                <div className="fs-6">Confirm Password</div>
                 <input id='password-confirm' type="password" class="form-control" placeholder="Type your Password"/>
             </div>
 
             <div id='picterUpload' style={{marginLeft: '22%'}}>
-                <div class="fs-6">Upload profile picture</div>
+                <div className="fs-6">Upload profile picture</div>
                 <input type="file" accept="image/*"/>
             </div>
 
-            <p></p>
-            <button id='loginButton' class="btn btn-primary" type="button" onClick={validation}>Register</button>
+            <button id='loginButton' className="btn btn-primary" type="button" onClick={validation} style={{marginTop: "3%"}}>Register</button>
 
             <div id="error-message"></div>
-
 
         </div>
     );
