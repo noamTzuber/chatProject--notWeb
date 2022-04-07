cc23d53cbd7a80f49e94f928bcb3f252dcd43a79
itayshwartz1 <itay2007@gmail.com>
2022-04-07 18:51:43 +0300

    itay do all the work

--- src/App.js
+++ src/App.js
@@ -9,19 +9,16 @@ import Register from './register/Register';
 import Login from './login/Login';
 import Chat from './chat/Chat';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-/**
- * do a clip -for photo and records
- * fix pixsels in login and register
- * validation in the first register
- * send text img record
- * change conversaions
- * bottom for add chat
- */
+
 function App() {
   return (
-    <div>
-      <Chat/>
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path = "/" element={<Login/>}/>
+              <Route path = "/Register" element={<Register/>}/>
+              <Route path = "/Chat/:id" element={<Chat/>}/>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
