7267dbfedc3c1e302c4d4970c6edef5a71a9a5e7
noamTzuber <noamtooby@gmail.com>
2022-04-08 14:13:27 +0300

    open files with pic

--- src/chat/Btn/Btn.js
+++ src/chat/Btn/Btn.js
@@ -39,44 +39,57 @@ function Btn(props) {
                     <button onClick={() => {
                         send(props.contact)
                     }} className="msg_send_btn" type="button">
-                        <i className="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+                        <i className="fa fa-paper-plane-o" style={{color:"black"}} aria-hidden="true"></i></button>
                         <span>
                              <div className="btn-group">
-  <button type="button" className="btn myfunction btn-prim " data-toggle="dropdown" aria-haspopup="true"
+
+  <button type="button" id="actual-btn" className="btn myfunction btn-prim " data-toggle="dropdown" aria-haspopup="true"
           aria-expanded="false">
     <i className="fa fa-paperclip" aria-hidden="true"></i>
 
   </button>
   <div className="dropdown-menu">
       {/*  pic icon*/}
-    <a className="dropdown-item item" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-    fill="currentColor" className="bi bi-image-fill" viewBox="0 0 16 16">
-  <path
-      d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2V3zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0z"/>
-</svg></a>
+    < button input type="file" accept="image/*" className="fs-6 dropdown-item item" >
+        <div  >
+
+<div className="upload-btn-wrapper">
+  <button className="btn-img"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                   fill="currentColor" className="bi bi-image-fill" viewBox="0 0 16 16">
+
+        <path
+            d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2V3zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0z"/>
+</svg></button>
+  <input type="file" name="myfile"/>
+</div>
+            </div>
+
+
+
+    </button>
       {/*  record icon*/}
-    <a className="dropdown-item"  href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+    <button className="dropdown-item"  href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
 fill="currentColor" className="bi bi-mic-fill" viewBox="0 0 16 16">
   <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z"/><path
       d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5
        4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/>
-</svg></a>
+</svg></button>
 
       {/*  video icon*/}
-    <a className="dropdown-item" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+    <button className="dropdown-item" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
    fill="currentColor" className="bi bi-camera-reels-fill" viewBox="0 0 16 16">
   <path d="M6 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
   <path d="M9 6a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
   <path
       d="M9 6h.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 7.269v7.462a1 1 0 0
       1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 16H2a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h7z"/>
-</svg> </a>
+</svg> </button>
       {/*  gps icon*/}
     <div className="dropdown-divider"></div>
-    <a className="dropdown-item" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+    <button className="dropdown-item" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
    fill="currentColor" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
   <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-</svg></a>
+</svg></button>
   </div>
 </div>
                         </span>
