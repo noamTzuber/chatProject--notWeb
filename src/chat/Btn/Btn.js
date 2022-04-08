6367548c2edfd1ad7126c83da2204d9c58aed761
noamTzuber <noamtooby@gmail.com>
2022-04-08 10:46:35 +0300

    add icons and dropups

--- src/chat/Btn/Btn.js
+++ src/chat/Btn/Btn.js
@@ -27,6 +27,7 @@ function Btn(props) {
     }
 
 
+
     return (
 
         <div className='bottom-bar'>
@@ -49,12 +50,9 @@ function Btn(props) {
   <div className="dropdown-menu">
       {/*  pic icon*/}
     <a className="dropdown-item item" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-  fill="currentColor" className="bi bi-images" viewBox="0 0 16 16">
-  <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/><path
-      d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2
-       0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1
-       0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .
-       577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z"/>
+    fill="currentColor" className="bi bi-image-fill" viewBox="0 0 16 16">
+  <path
+      d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2V3zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0z"/>
 </svg></a>
       {/*  record icon*/}
     <a className="dropdown-item" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
@@ -63,6 +61,7 @@ fill="currentColor" className="bi bi-mic-fill" viewBox="0 0 16 16">
       d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5
        4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/>
 </svg></a>
+
       {/*  video icon*/}
     <a className="dropdown-item" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
    fill="currentColor" className="bi bi-camera-reels-fill" viewBox="0 0 16 16">
