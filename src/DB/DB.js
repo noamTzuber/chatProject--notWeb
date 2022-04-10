83d049e1d0743e8cb44049ce108791b5aef9b475
itayshwartz1 <itay2007@gmail.com>
2022-04-10 14:43:49 +0300

    add new users work. need validation

--- src/DB/DB.js
+++ src/DB/DB.js
@@ -40,8 +40,50 @@ export const users = [{
             text: [{txt: 'hey my name is yehuda', isIncoming: 0, time: "20:29"}, {txt: 'me', isIncoming: 1, time: "20:29"}]
         }
     ]
-},
+},{
 
+    name: 'noam',
+    nickName: 'itayos',
+    password: 'itay1234',
+    profilePic: "ron.jpg",
+    chats: [
+    {
+        contact: 'harry',
+        img: "harry.jpg",
+        lastMessage: 'bye',
+        lastTime: '9:00',
+        text: [{txt: 'hey my name is noam', isIncoming: 0, time: "20:29"}, {txt: 'bye', isIncoming: 1 , time: "20:29"}]
+    },
+    {
+        contact: 'hermione',
+        img: "hermione.jpg",
+        lastMessage: 'bye1',
+        lastTime: '10:00',
+        text: [{txt: 'hey my name is dvir', isIncoming: 0, time: "20:29"}, {txt: 'me', isIncoming: 1, time: "20:29"}]
+    },
+    {
+        contact: 'ron',
+        img: "ron.jpg",
+        lastMessage: 'bye2',
+        lastTime: '11:00',
+        text: [{txt: 'hey my name is inbal', isIncoming: 0, time: "20:29"}, {txt: 'me', isIncoming: 1, time: "20:29"}]
+    },
+    {
+        contact: 'voldemort',
+        img: "voldemort.jpeg",
+        lastMessage: 'byeGil',
+        lastTime: '11:00',
+        text: [{txt: 'hey my namr is gilad', isIncoming: 0, time: "20:29"}, {txt: 'me', isIncoming: 1, time: "20:29"}]
+    },
+    {
+        contact: 'dumbledore',
+        img: "dumbledore.jpg",
+        lastMessage: 'bye2Ye',
+        lastTime: '11:00',
+        text: [{txt: 'hey my name is yehuda', isIncoming: 0, time: "20:29"}, {txt: 'me', isIncoming: 1, time: "20:29"}]
+    }
+]
+}
 
 ]
 export default users
